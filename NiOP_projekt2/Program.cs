e4d4b3a901be4ac6c9e48d28cdb373d5aa252cbc
HennHox <henn.hox@gmail.com>
2025-01-19 16:47:22 +0100

    LINQ

--- NiOP_projekt2/Program.cs
+++ NiOP_projekt2/Program.cs
@@ -19,6 +19,7 @@ namespace NiOP_projekt2
                 Console.WriteLine("1 - Dodaj korisnika");
                 Console.WriteLine("2 - Obrisi korisnika");
                 Console.WriteLine("3 - Ispis svih korisnika");
+                Console.WriteLine("4 - Pretraga korisnika");
                 Console.WriteLine("0 - Izlaz");
                 Console.Write("Vaš izbor: ");
 
@@ -34,6 +35,9 @@ namespace NiOP_projekt2
                     case "3":
                         IspisiSveKorisnike(upravljanje);
                         break;
+                    case "4":
+                        PretragaKorisnika(upravljanje);
+                        break;
                     case "0":
                         izlaz = true;
                         Console.WriteLine("Izlaz iz programa...");
@@ -44,6 +48,68 @@ namespace NiOP_projekt2
                 }
             }
         }
+
+        static void PretragaKorisnika(UpravljanjeKorisnicima upravljanje)
+        {
+            Console.WriteLine("\nOdaberite kriterij za pretragu:");
+            Console.WriteLine("1 - Pretraga prema ID-u");
+            Console.WriteLine("2 - Pretraga prema imenu");
+            Console.WriteLine("3 - Pretraga prema prezimenu");
+            Console.WriteLine("4 - Pretraga prema kontaktu");
+            Console.Write("Vaš izbor: ");
+
+            string izbor = Console.ReadLine();
+            Console.Write("Unesite vrijednost za pretragu: ");
+            string vrijednost = Console.ReadLine();
+
+            var korisnici = upravljanje.DohvatiSveKorisnike();
+            var rezultati = Enumerable.Empty<Korisnik>();
+
+            switch (izbor)
+            {
+                case "1":
+                    if (int.TryParse(vrijednost, out int id))
+                    {
+                        rezultati = korisnici.Where(k => k.ID == id);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unesite ispravan broj za ID.");
+                        return;
+                    }
+                    break;
+
+                case "2":
+                    rezultati = korisnici.Where(k => k.Ime.IndexOf(vrijednost, StringComparison.OrdinalIgnoreCase) >= 0);
+                    break;
+
+                case "3":
+                    rezultati = korisnici.Where(k => k.Prezime.IndexOf(vrijednost, StringComparison.OrdinalIgnoreCase) >= 0);
+                    break;
+
+                case "4":
+                    rezultati = korisnici.Where(k => k.Kontakt.IndexOf(vrijednost, StringComparison.OrdinalIgnoreCase) >= 0);
+                    break;
+
+                default:
+                    Console.WriteLine("Nepoznata opcija. Povratak u glavni izbornik.");
+                    return;
+            }
+
+            if (rezultati.Any())
+            {
+                Console.WriteLine("\nRezultati pretrage:");
+                foreach (var korisnik in rezultati)
+                {
+                    Console.WriteLine(korisnik);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nema rezultata za navedeni kriterij.");
+            }
+        }
+
 
         static void DodajNovogKorisnika(UpravljanjeKorisnicima upravljanje)
         {
